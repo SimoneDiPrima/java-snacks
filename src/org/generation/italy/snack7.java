e085e65054a4570e875c9717301b2b2dfaa13bda
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-11-16 17:52:04 +0100

    second commit

--- src/org/generation/italy/snack7.java
+++ src/org/generation/italy/snack7.java
@@ -5,13 +5,20 @@ public class snack7 {
 	public static void main(String[] args) {
 		String[] words = { "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit" };
 		String Sentence = "";
+		String maxSentence = "";
 		for(int i =0;i<words.length;i++) {
 			Sentence += words[i];
+			if(words[i].length()>maxSentence.length()) {
+				maxSentence = words[i];
+			}
+			
 		}
 		System.out.println(Sentence);
 		StringBuilder strb = new StringBuilder(Sentence);
 		Sentence = strb.reverse().toString();
 		System.out.println(Sentence);
+		System.out.println("la parola piu lunga è " + maxSentence);
+		
 	}
 
 }
