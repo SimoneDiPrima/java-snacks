310e7c2119e3c7017630964c3c80cc3ddbc7b03c
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-11-16 17:04:25 +0100

    second commit

---
+++ src/org/generation/italy/snack7.java
@@ -0,0 +1,14 @@
+package org.generation.italy;
+
+public class snack7 {
+
+	public static void main(String[] args) {
+		String[] words = { "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit" };
+		String Sentence = "";
+		for(int i =0;i<words.length;i++) {
+			Sentence += words[i];
+		}
+		System.out.println(Sentence);
+	}
+
+}
