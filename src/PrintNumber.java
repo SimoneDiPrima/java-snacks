6ba9f90fed9f149d32a5bedd98ae56cfe2b41134
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-11-16 10:14:29 +0100

    first commit

---
+++ src/PrintNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrintNumber {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.print("dimmi un numero da 1 a 100:");
+		 int num = sc.nextInt();
+		 if(num%2 == 0) {
+			 System.out.print("il numero scelto è pari");
+		 }
+		 else {
+		 
+			 System.out.print(num+1);
+
+	}
+
+}}
+
+
+
+
