6494dc9f953eb6c64ed17fd4c1375a6e02ca04b5
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-11-16 16:59:29 +0100

    second commit

--- src/org/generation/italy/ManageArrayFirst.java
+++ src/org/generation/italy/ManageArrayFirst.java
@@ -4,15 +4,17 @@ public class ManageArrayFirst{
 
 	public static void main(String[] args) {
 		int[] arr = { 70, 39, 18, 30, 51, 59, 1, 47, 67, 87 };
+		int minValue =1;
+		int maxValue =87;
 		int sum =0;
 		for(int i = 0; i < arr.length;i++) {
 			int min = arr[0 + i];
 			int max = arr[arr.length - 1];
-			if(min<i){
-			System.out.println("il valore minimo è " + min);
+			if(minValue<=arr[i]){
+			System.out.println("il valore minimo è " + minValue);
 			}
-			if(arr[arr.length - 1] > arr[i]){
-				System.out.println("il valore massimo è " + max);
+			if(maxValue>=arr[i]){
+				System.out.println("il valore massimo è " + maxValue);
 			}
 			sum += arr[i];
 			
--- src/org/generation/italy/snack6.java
+++ src/org/generation/italy/snack6.java
@@ -8,15 +8,25 @@ public class snack6 {
 		int[] arr2 = { 42, 56, 7, 42, 62, 90, 86, 54, 45, 32 };
 		int[] arr3 = new int[arr1.length];
 		int sum=0;
+		int minValue = 100;
+		int maxValue = 0;
 		
 		
 		for (int i = 0; i < arr1.length; ++i) {
 		    arr3[i] = arr1[i] + arr2[i];
 		    System.out.println(arr3[i]);
 		    sum += (arr1[i] + arr2[i]);
+		    if(arr1[i]<minValue) {
+		    	minValue= arr1[i];
+		    }
+		    else if(maxValue <= arr2[i]) {
+		    	maxValue = arr2[i];
+		    }
 		}
 		
 		 System.out.println("il valore totale è: "+sum );
+		 System.out.println("il valore massimo è: "+maxValue );
+		 System.out.println("il valore minimo è: "+minValue );
 		
 	}
 	
